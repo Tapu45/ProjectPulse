7bbf95b3c3d2b60c440919158046e2b84493d383
Tapu45 <rameswarpanda891@gmail.com>
2025-05-17 00:27:46 +0530

    first commit7

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ const port = 3000;
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173', 'http://127.0.0.1:5174', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5174', 'http://localhost:5173', 'http://127.0.0.1:5174', 'http://127.0.0.1:5173', '*'],
   credentials: true
 }));
 
